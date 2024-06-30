e17cd8a9d8bebe15c1eebae18240c1c7d909d802
Linterz <linzhi@msu.edu>
2024-06-30 19:07:03 -0400

    added options for 12 and 24 hours

--- src/plugins/fullDateTime/index.tsx
+++ src/plugins/fullDateTime/index.tsx
@@ -4,26 +4,53 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
-import definePlugin from "@utils/types";
+import definePlugin, { OptionType } from "@utils/types";
+
+const enum Methods {
+    twelveHour,
+    twentyFourHour,
+}
+
+const settings = definePluginSettings({
+    method: {
+        description: "Time format",
+        type: OptionType.SELECT,
+        options: [
+            { label: "12 hour", value: Methods.twelveHour, default: true },
+            { label: "24 hour", value: Methods.twentyFourHour },
+        ],
+    },
+});
 
 export default definePlugin({
     name: "FullDateTimePlugin",
     description: "Show full time for messages in 24-hour format",
     authors: [Devs.Nobody],
+    settings,
 
     patches: [
         {
             find: ".Messages.MESSAGE_EDITED_TIMESTAMP_A11Y_LABEL",
             replacement: [{
                 match: /(\(d,\s*"LT")/,
-                replace: "(d, 'HH:mm:ss'"
+                replace: (_, orig) => `(d, "${format()}"`
             },
             {
                 match: /(\(d,\s*"LLLL")/,
-                replace: "(d, 'YYYY-MM-DD HH:mm:ss'"
+                replace: (_, orig) => `(d, "YYYY-MM-DD ${format()}"`,
             }]
-        }
-    ]
+        },
+    ],
 },
 );
+
+function format() {
+    switch (settings.store.method) {
+        case Methods.twelveHour:
+            return "h:mm:ss A";
+        case Methods.twentyFourHour:
+            return "HH:mm:ss";
+    }
+}
